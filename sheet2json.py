845db3f1ef2bd9f4830156fc135e2173d4f1b679
NetAcad DEVASC <changeme@example.com>
2021-05-13 12:15:26 +0000

    update room to meeting

--- sheet2json.py
+++ sheet2json.py
@@ -3,11 +3,11 @@ def sheet2json(sheet):
     from pandas import read_excel
     df = read_excel(sheet,sheet_name=0)
     d = {}
-    for room in df['name_room'].unique():
-        d[room] = [{
+    for meeting in df['name_meeting'].unique():
+        d[meeting] = [{
         'fn': df['fn_participant'][item],
         'sn': df['sn_participant'][item],
         'email': df['email_participant'][item]} 
-        for item in df[df['name_room']==room].index]
+        for item in df[df['name_meeting']==meeting].index]
     d_json = json.dumps(d)
     return d_json
