0bdb577044205001d257c97c4e14c6553b85dbab
NetAcad DEVASC <changeme@example.com>
2021-05-30 21:07:08 +0200

    v1

--- meeting_info.py
+++ meeting_info.py
@@ -1,15 +1,11 @@
 import requests
 import json
-from environment import biasc as env
-#from environment import int_report as env
+#from environment import biasc as env
+from environment import int_report as env
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta as delta
 import pandas as pd
-
-access_token = env['token']
-headers = {
-    'Authorization': 'Bearer {}'.format(access_token)
-}
+from urllib.parse import urlencode
 
 def get_emails_excel(emails):
     df = pd.DataFrame()
@@ -25,60 +21,71 @@ def get_emails(pages):
     return list_mails
 
 
-def list_people():
-    url = "https://webexapis.com/v1/people" 
-    first_page = requests.get(url,headers=headers)
-    yield first_page.json()
-    if 'next' in first_page.links:
-        page = first_page.links['next']['url']
-        while page:
-            next_page = requests.get(page,headers=headers)
-            yield next_page.json()
-            if 'next' in next_page.links:
-                page = next_page.links['next']['url']
-            else:
-                page = False
-
+def pages_list(url):
+    f_page = requests.get(url,headers=env['headers'])
+    if f_page.status_code==200:
+        yield f_page.json()
+        if 'next' in f_page.links:
+            page = f_page.links['next']['url']
+            while page:
+                n_page = requests.get(page,headers=env['headers'])
+                if n_page.status_code==200:
+                    yield n_page.json()
+                    if 'next' in n_page.links:
+                        page = n_page.links['next']['url']
+                    else:
+                        page = False
+    else:
+        print(f_page.reason)
 
 def count_part(meetingId):
     url = f"https://webexapis.com/v1/meetingParticipants?meetingId={meetingId}"
-    response = requests.get(url, headers=headers, data={})
+    response = requests.get(url, headers=env['headers'])
     if response.status_code==200:
         dict = json.loads(response.text)
         return (len(dict["items"]))
 
+def get_param(age_month):
+    date_d = delta(months=int('-'+str(age_month)))
+    date   = dt.today() + delta(days=+1)
+    param_dict = {
+        'to': date.strftime(format='%Y-%m-%d'),
+        'from': (date + date_d).strftime(format='%Y-%m-%d'),
+        'siteUrl': env['siteUrl'],
+        'max': 100
+    }
+    return param_dict
+    
+
+def get_meeting(m):
+    mt_dict = {
+    'id': m["id"],
+    'meetingNumber': m['meetingNumber'], 
+    'timezone': m['timezone'], 
+    'start': m['start'],
+    'end': m['end'], 
+    'hostUserId': m['hostUserId'],
+    'participants': count_part(m["id"])
+    }
+    return mt_dict
+
 def main():
     #people_list = pd.read_excel('people.xlsx')['people'].tolist()
-    people_list = get_emails(list_people())
-    url_meetings = "https://webexapis.com/v1/meetings"
-    #delta = delta(months=-3)
-    v_delta = delta(days=-10)
-    date = dt.today() + delta(days=+1)
-    date_to = date.strftime(format='%Y-%m-%d')
-    date_from = (date + v_delta).strftime(format='%Y-%m-%d')
-    siteUrl = env['siteUrl']
-    mt_list = []
-    for p in people_list:
-        hostEmail= p
-        uri = f"{url_meetings}?from={date_from}&to={date_to}&siteUrl={siteUrl}&hostEmail={hostEmail}"
-        response = requests.get(uri, headers=headers)
-        if response.status_code==200:
-            meetings_json = response.json()
-            for m in meetings_json['items']:
-                meetingId = m["id"]
-                mt_dict = {
-                    'id': meetingId,
-                    'meetingNumber': m['meetingNumber'], 
-                    'timezone': m['timezone'], 
-                    'start': m['start'],
-                    'end': m['end'], 
-                    'hostUserId': m['hostUserId'],
-                    'participants': count_part(meetingId)
-                }
-                mt_list.append(mt_dict)
-    df = pd.DataFrame(mt_list)
+    url = "https://webexapis.com/v1/"
+    people_list  = get_emails(pages_list(f'{url}people'))
+    param_dict = get_param(age_month=1)
+    mt_list    = []
+    for person in people_list:
+        param_dict['hostEmail'] = person
+        uri = f"{url}meetings?{urlencode(param_dict)}"
+        #response = requests.get(url+'meetings', headers=headers, params=param_dict)
+        pages = pages_list(uri)
+        for page in pages:
+            for mt in page["items"]:
+                mt_list.append(get_meeting(mt))
+    meetings_df = pd.DataFrame(mt_list).result.fillna(0, inplace=True, downcast='infer')
     #df.to_excel(f'meeting.xlsx',sheet_name='meetings',index=False)
-    df.to_excel(f'meeting_stats_{date_from}-{date_to}.xlsx',sheet_name='meetings',index=False)
+    meetings_df.to_excel(f'meeting_stats_{param_dict["from"]}-{param_dict["to"]}.xlsx',sheet_name='meetings',index=False)
 
 # execute main when called directly
 if __name__ == '__main__':
