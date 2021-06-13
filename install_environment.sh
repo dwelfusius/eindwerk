862e60c6af0f6054a6b82673c87fbf717d49354a
NetAcad DEVASC <changeme@example.com>
2021-06-14 00:00:06 +0200

    cleanup

--- install_environment.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-pip install pandas
-pip install openpyxl
-pip install webexteamssdk
-pip install webexteamssdk --upgrade
-git clone https://github.com/CiscoDevNet/webexteamssdk.git
-python setup.py install
